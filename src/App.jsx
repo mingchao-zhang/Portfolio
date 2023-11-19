5414ffa7ea94e87d0cd216a02c065deaafe76303
Mingchao Zhang <zmcmarkzhang@gmail.com>
2023-11-19 11:44:23 -0500

    install tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,13 @@ import { Experience } from "./components/Experience";
 
 function App() {
   return (
-    <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
-      <color attach="background" args={["#ececec"]} />
-      <Experience />
-    </Canvas>
+    <>
+      {/* <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
+        <color attach="background" args={["#ececec"]} />
+        <Experience />
+      </Canvas> */}
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    </>
   );
 }
 
